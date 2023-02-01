dc2acb4a29907dd9a698e63242adbe924a17a6ff
Dericrandomusername <107108479+Dericrandomusername@users.noreply.github.com>
2023-02-01 17:09:07 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    Canvas=createCanvas(400, 400);
+    Canvas.center()
+}
+function draw(){
+    
+}
+function takeSnapshot(){
+    save("filterimage.png")
+}
